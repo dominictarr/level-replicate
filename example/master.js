e8979625a3500da277230d64db694f514fe979f6
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-05 19:05:03 +0100

    examples

--- example/master.js
+++ example/master.js
@@ -3,7 +3,9 @@ var SubLevel = require('level-sublevel')
 var net      = require('net')
 var Master   = require('../')
 var name     = require('random-name')
-var LiveStream = require('level-live-stream')
+
+var pull     = require('pull-stream')
+var pl       = require('pull-level')
 
 var db = SubLevel(levelup('/tmp/example-master'))
 
@@ -11,24 +13,34 @@ var master = Master(db, 'master')
 
 //createServer(db, master)
 net.createServer(function (stream) {
-  stream.pipe(master.createStream({tail: true})).pipe(stream)
-  stream.on('close', function () {
-    console.log('DISCONNECT')
+  var ms
+  stream.pipe(ms = master.createStream({tail: true})).pipe(stream)
+  stream.on('end', function () {
+//    ms.destroy()
+    console.log('END')
   })
 }).listen(9999, function () {
   console.log('master db listening on 9999')
 })
 
-db.post(console.log)
-//master.post(console.log)
+//db.post(console.log)
+master.post(console.log)
 
-LiveStream(master, {start: '0',
-tail: false}).on('data', console.log)
-
-setInterval(function () {
+var i = 0
+var ts = 0
+var int = setInterval(function () {
+  ts = ts || Date.now()
   var key = name()
   var val = new Date().toString()
   db.put(key, val, function (err) {
     if(err) throw err
   })
+
+  if(i++ > 3) {
+//    clearInterval(int)
+    return
+    master.createPullStream({tail: true, since: ts})
+//    pl.read(db, {tail: true})
+      .pipe(pull.log())
+  }
 }, 1000)
--- example/slave.js
+++ example/slave.js
@@ -8,15 +8,18 @@ var slave = Master.Slave(db, 'slave')
 
 var stream = net.connect(9999)
 
-//stream.pipe(slave.createStream()).pipe(stream)
 
-stream
-.on('data', function (data) {
-  console.log('>', data.toString())
-})
+stream.pipe(slave.createStream()).on('data', console.log).pipe(stream)
 
-stream.write('{"since": 0}\n\n')
-// log inserts
 db.post(console.log)
+
+//stream
+//.on('data', function (data) {
+//  console.log('>', data.toString())
+//})
+
+//stream.write('{"since": 0}\n\n')
+// log inserts
+//db.post(console.log)
 
 
