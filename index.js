6b5f51677fba2f74b3719c6710b4c85ef8dfa90d
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-05 23:11:56 +0200

    implement simple replication

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var timestamp = require('monotonic-timestamp')
 var pull      = require('pull-stream')
 var ClassicStream
@@ -6,18 +5,75 @@ var ClassicStream
 var serialize = require('stream-serializer')()
 var window    = require('pull-window')
 var pl        = require('pull-level')
+var merge     = require('pull-stream-merge')
+var cat       = require('pull-cat')
+var lookup    = require('./lookup')
 
-exports = module.exports = function (db, master) {
+function each (obj, iter) {
+  for(var key in obj) {
+    iter(obj[key], key, obj)
+  }
+}
 
-  master = master || 'master'
-  if('string' === typeof master)
-    master = db.sublevel(master)
+function map (obj, iter) {
+  var a = []
+  for(var k in obj)
+    a.push(iter(obj[k], k, obj))
+  return a
+}
+
+function find (ary, test) {
+  for(var i in ary)
+    if(test(ary[i], i, ary)) return ary[i]
+}
+
+function filterReverse (ary, test) {
+  var l = ary.length, a = []
+  while(l--)
+    if(test(ary[l], l, ary)) a.push(ary[l])
+
+  return a
+}
+
+function flatten (ary) {
+  return ary.reduce(function (ary, _ary) {
+    return ary.concat(_ary)
+  }, [])
+
+}
+
+function cmp (a, b) {
+  return a === b ? 0 : a < b ? -1 : 1
+}
+
+function comparator(a, b) {
+  var _a = a.key.split('!'), _b = b.key.split('!')
+  console.log(cmp(_a[1], _b[1]) || cmp(_a[0], _b[0]))
+  return cmp(_a[1], _b[1]) || cmp(_a[0], _b[0])
+}
+
+exports = module.exports = function (db, masterDb, id) {
+
+  masterDb = masterDb || 'master'
+  if('string' === typeof masterDb)
+    masterDb = db.sublevel(masterDb)
+  var clockDb = masterDb.sublevel('clock')
 
   //ADD A LOG THAT POINTS TO WHICH KEYS WHERE UPDATED WHEN.
   
-  db.pre(function (ch, add) {
-    add({key: timestamp(), value: ch.key, type: 'put'}, master)
-  })  
+  db.pre(function (op, add, batch) {
+    console.error('PRE', batch)
+    
+    if(!find(batch, function (_op) {
+        return _op.value === op.key && _op.prefix === masterDb
+      })
+    ) {
+      var ts = timestamp()
+      console.log("INSERT", ts)
+      add({key: id+'!'+ts, value: op.key, type: 'put', prefix: masterDb})
+      add({key: id, value: ''+ts, type: 'put', prefix: clockDb})
+    }
+  })
 
   //cleanup old records.
   //run this every so often if you have lots of overwrites.
@@ -27,23 +83,25 @@ exports = module.exports = function (db, master) {
   //group into batches, so it's more efficient communication with db,
   //and delete each set.
 
-  master.cleanup = function (cb) {
-      pl.read(db, {reverse: true})
-      .pipe(pull.nonUnique(function (d) {
-        return d.value.toString()
-      }))
-      .pipe(pull.map(function () {
-        return {key: d.key, type: 'del'}
-      }))
-      .pipe(pl.write(db, cb))
+  var clock = {}
+
+  masterDb.cleanup = function (cb) {
+    pl.read(db, {reverse: true})
+    .pipe(pull.nonUnique(function (d) {
+      return d.value.toString()
+    }))
+    .pipe(pull.map(function () {
+      return {key: d.key, type: 'del'}
+    }))
+    .pipe(pl.write(db, cb))
   }
 
-  master.createStream = function (opts) {
+  masterDb.createStream = function (opts) {
     var defer = pull.defer()
     var cs = ClassicStream(function (read) {
         read(null, function (err, data) {
-          defer.resolve(master.createPullStream({
-            min: String(data.since || 0),
+          defer.resolve(masterDb.createMasterStream({
+            min: String(id+'!'+(data.since || 0)),
             tail: opts.tail
           }))
         })
@@ -52,16 +110,51 @@ exports = module.exports = function (db, master) {
     return serialize(cs)
   }
 
-  master.createPullStream = function (opts, onAbort) {
+  masterDb.createStream = function () {
+    var defer = pull.defer()
+    var cs = ClassicStream(function (read) {
+        read(null, function (err, data) {
+          defer.resolve(masterDb.createMasterStream({
+            min: String(id+'!'+(data.since || 0)),
+            tail: opts.tail
+          }))
+        })
+      }, defer)
+
+    return serialize(cs)
+  }
+
+  
+  masterDb.createMasterStream = pull.Source(function (opts, onAbort) {
     opts = opts || {}
+    opts.clock = opts.clock || {}
     var since = opts.min || opts.since || 0
-    opts.min = String(since)
-    //read a header, and then send the data...
-    return pl.read(master, opts)
-      .pipe(pull.filter(function (data) {
-        //filter deletes, this is just cleaning up!
-        return (since < data.key && !!data.value)
-      }))
+
+    function rest (clock) {
+
+      var nClock = {}
+      each(clock, function (_, key) {
+        nClock[key] = 0
+      })
+
+      each(opts.clock, function (value, key) {
+        if(nClock[key] < value)
+          nClock[key] = value
+      })
+
+      return cat([
+        merge(map(nClock, function (value, key) {
+          return {min: key + '!' + value, max: key+'!~', tail: false}
+        }).map(function (opts) {
+          return pl.read(masterDb, opts)
+            //can remove this once level gets exclusive ranges!
+            .pipe(pull.filter(function (data) {
+              var c = nClock[data.key]
+              return !c || (c < data.key) && !!data.value
+            }))
+        }), comparator),
+        opts.tail ? pl.live(masterDb) : pull.empty()
+      ])
       .pipe(pull.asyncMap(function (data, cb) {
          db.get(data.value, function (err, value) {
             cb(null, {
@@ -84,65 +177,148 @@ exports = module.exports = function (db, master) {
           })
         }
       })
+    }
+
+    var read
+
+    return function (abort, cb) {
+      if(!read)
+        masterDb.clock(function (err, nClock) {
+          if(err) return cb(err)
+          console.error('REST?', nClock, opts)
+          ;(read = rest(nClock))(abort, cb)
+        })
+      else
+        read(abort, cb)
+    }
+  })
+
+  masterDb.clock = function (cb) {
+    pl.read(clockDb)
+      .pipe(pull.reduce(function (clock, item) {
+        clock[item.key] = item.value
+        console.error('ITEM', clock)
+        return clock
+      }, {}, cb))
   }
-  return master
-}
 
 
-exports.slave = exports.Slave = function (db, slave) {
-
-  slave = slave || 'slave'
-  if('string' === typeof slave)
-    slave = db.sublevel(slave)
-
-  slave.since = function (cb) {
-    slave.get('seq', function (err, val) {
-      cb(null, Number(val) || 0)
+  //TODO: defer opening database until this is loaded!
+  //this requires a patch to level!
+  masterDb.clock(function (err, _clock) {
+    each(clock, function (ts, id) {
+      if(clock[id] < ts)
+        clock[id] = ts
     })
-  }
+  })
 
-  slave.createStream = function () {
-    var cs = serialize(ClassicStream())
-    cs.source.pipe(
-      slave.createPullStream(function (err) {
-        if(err) cs.emit('error')
-        else cs.emit('close')
-    }))
-    var first = true
-    cs.sink(function (end, cb) {
-      if(!first) return
-      first = false
-      slave.since(function (err, ts) {
-        console.log(ts)
-        cb(null, {since: ts})
-      })
-    })
-    return cs
-  }
+  clockDb.post(function (op) {
+    if(clock[op.key] < op.value)
+      clock[op.key] = op.value
+  })
 
-  slave.createPullStream = function (opts, done) {
+  //the writable side of the replication stream.
+
+  masterDb.createSlaveStream = function (opts, done) {
     if('function' === typeof opts) {
       done = opts; opts = null
     }
 
-    return window(10, 100) //read from remote MASTER
-      .pipe(pull.map(function (batch) {
-        var max = 0
-        batch.forEach(function (e) {
-          if(e.ts > max)
-            max = e.ts
-        })
-        batch.push({
-          type: 'put', key: slave.prefix('seq'), value: max
-        })
-        return batch
-      }))
-      .pipe(pull.asyncMap(function (batch, cb) {
-        db.batch(batch, function (err) {
-          cb(err, batch)
-        })
-      }))
-      .pipe(pull.drain(null, done))
+    return pull.map(function (op) {
+      var parts = op.ts.split('!')
+      var _id = parts.shift()
+      var ts  = parts.shift()
+
+      if(clock[_id] > ts) return
+
+      return [
+        op,
+        {key: op.ts, value: op.key, type: 'put', prefix: masterDb},
+        {key: _id, value: ts, type: 'put', prefix: clockDb}
+      ]
+
+    }).pipe(pull.filter(Boolean))
+    .pipe(window(10, 100))
+    .pipe(pull.map(function (_batch) {
+
+      var batch = flatten(_batch)
+
+      var seen = {}
+      //make sure there is only one clock update per batch
+      return filterReverse(batch, function (op) {
+        if(op.prefix !== clockDb) return true
+        else if (!seen[op.key])    return seen[op.key] = true
+        return false
+      })
+    }))
+    .pipe(pull.asyncMap(function (batch, cb) {
+      db.batch(batch, function (err) {
+        console.log('saved', err)
+        cb(err, batch)
+      })
+    }))
+    .pipe(pull.drain(null, done))
   }
-  return slave
+
+  return masterDb
 }
+
+
+//exports.slave = exports.Slave = function (db, slave) {
+//
+//  slave = slave || 'slave'
+//  if('string' === typeof slave)
+//    slave = db.sublevel(slave)
+//
+//  slave.since = function (cb) {
+//    slave.get('seq', function (err, val) {
+//      cb(null, Number(val) || 0)
+//    })
+//  }
+//
+//  slave.createStream = function () {
+//    var cs = serialize(ClassicStream())
+//    cs.source.pipe(
+//      slave.createPullStream(function (err) {
+//        if(err) cs.emit('error')
+//        else cs.emit('close')
+//    }))
+//    var first = true
+//    cs.sink(function (end, cb) {
+//      if(!first) return
+//      first = false
+//      slave.since(function (err, ts) {
+//        console.log(ts)
+//        cb(null, {since: ts})
+//      })
+//    })
+//    return cs
+//  }
+//
+//  slave.createPullStream = function (opts, done) {
+//    if('function' === typeof opts) {
+//      done = opts; opts = null
+//    }
+//
+//    return window(10, 100) //read from remote MASTER
+//      .pipe(pull.map(function (batch) {
+//        var max = 0
+//        batch.forEach(function (e) {
+//          if(e.ts > max)
+//            max = e.ts
+//        })
+//        batch.push({
+//          type: 'put', key: slave.prefix('seq'), value: max
+//        })
+//        return batch
+//      }))
+//      .pipe(pull.asyncMap(function (batch, cb) {
+//        db.batch(batch, function (err) {
+//          cb(err, batch)
+//        })
+//      }))
+//      .pipe(pull.drain(null, done))
+//  }
+//
+//  return slave
+//}
