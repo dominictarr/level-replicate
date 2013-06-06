a040910db4f96481d1567de36af8a4415dba0feb
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-06 16:08:32 +0200

    use all tests

--- test/replicate.js
+++ test/replicate.js
@@ -21,6 +21,11 @@ var master = Master(db, 'master', 'T1')
 
 slave = Master(_db, 'master', 'T2')
 
+process.on('uncaughtException', function (err) {
+  console.error(err.stack || err)
+  process.exit(1)
+})
+
 test('setup', function (t) {
   help.populate(db, 100, function (err) {
     t.notOk(err)
@@ -40,9 +45,10 @@ test('createStream', function (t) {
     console.log('PULL STREAM SINCE:', clock)
     master.createMasterStream()
       .pipe(pull.through(function (data) {
-        t.ok(data.ts > clock, data.ts + ' > ' + clock)
+        t.ok(data.ts > (clock[data.id] || 0), data.ts + ' > ' + (clock[data.id] || 0))
       }))
       .pipe(slave.createSlaveStream(function (err) {
+        t.notOk(err)
         //*********************************
         //SOME TIMES THIS DOESN"T HAPPEN????
         //*********************************
@@ -51,7 +57,6 @@ test('createStream', function (t) {
   })
 })
 
-return
 test('updates', function (t) {
   help.populate(db, 100, function (err) {
     t.notOk(err)
@@ -63,12 +68,10 @@ test('createStream2', function (t) {
 
   slave.clock(function (err, clock) {
 
-    t.notEqual(since, 0)
-
-    console.log('PULL STREAM SINCE:', since)
+    console.log('PULL STREAM SINCE:', clock)
     master.createMasterStream({clock: clock})
       .pipe(pull.through(function (data) {
-        t.ok(data.ts > since, data.ts + ' > ' + since)
+        t.ok(data.ts > (clock[data.id] || 0), data.ts + ' > ' + clock[data.id])
       }))
       .pipe(slave.createSlaveStream(function (err) {
         if(err) throw err
