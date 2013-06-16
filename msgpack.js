c23d27ccd762c8544e059757bff055ee428decf5
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-16 23:36:40 +0100

    implement binary

---
+++ msgpack.js
@@ -0,0 +1,13 @@
+var inject = require('./inject')
+var combine = require('stream-combiner')
+var msgpack = require('msgpack-stream')
+
+function wrap (stream) {
+  return combine(
+    msgpack.createDecodeStream(),
+    stream,
+    msgpack.createEncodeStream()
+  )
+}
+
+module.exports = inject(wrap)
