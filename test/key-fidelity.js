1276817bcee0327666e908063cea2267f1d2e128
Nicholas Westlake <nicholasredlin@gmail.com>
2014-03-14 23:58:41 -0500

    eliminate race condition

--- test/key-fidelity.js
+++ test/key-fidelity.js
@@ -33,7 +33,7 @@ test('fills', function (t) {
 // store the data that is sent from one master to the other
 var wireData = {}
 
-run = function (key) {
+run = function (key, expectedWireCalls) {
   test('replicates', function (t) {
     db1 = sublevel(levelup('key-fidelity_db1', opts))
     db2 = sublevel(levelup('key-fidelity_db2', opts))
@@ -41,40 +41,52 @@ run = function (key) {
     m2 = replicate(db2, 'master', 'M4')
 
     // replicate databases
-    m1s = m1.createStream({tail: true})
-    m1s.pipe(m2.createStream({tail: true})).pipe(m1s)
+    var m1s = m1.createStream({tail: true})
+    var m2s = m2.createStream({tail: true})
+
+    m1s.pipe(m2s).pipe(m1s)
     m1s.on('data', function(data){
       wireData[key] = wireData[key] || {};
       wireData[key][data] = true;
     })
 
-    setTimeout(function () {
+    var wireCallTimes = 1;
+
+    m2s.on('data', function () {
+      console.log('data', wireCallTimes, expectedWireCalls)
+      if (wireCallTimes < expectedWireCalls) return wireCallTimes++;
       // collect result
       var db1result = ''
       var db2result = ''
 
-      db1.createReadStream().on('data', function(data){
-        db1result += JSON.stringify(data)
-      })
-      db2.createReadStream().on('data', function(data){
-        db2result += JSON.stringify(data)
-      })
+      var s1 = db1.createReadStream()
+      var s2 = db2.createReadStream()
 
-      setTimeout(function () {
+      s1.on('data', function(data){ db1result += JSON.stringify(data) })
+      s2.on('data', function(data){ db2result += JSON.stringify(data) })
+
+      s2.on('end', done)
+      s1.on('end', done)
+
+      var doneTimes = 0
+      function done() {
+        if (doneTimes < 1) return doneTimes++;
+        console.log('done', doneTimes)
+
         t.ok(db1result, 'db1 has content')
         t.equal(db1result, db2result, 'contents matches')
 
         db1.close(function(){ db2.close(function(){ t.end() }) })
-      }, 100)
-    }, 100)
+      }
+    })
   })
 }
 
 // replicate several times
-run('1')
-run('2')
-run('3')
-run('4')
+run('1', 2)
+run('2', 1)
+run('3', 1)
+run('4', 1)
 
 test('only info about key "foo" is sent', function(t){
   keys = []
