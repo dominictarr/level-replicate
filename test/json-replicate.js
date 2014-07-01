aa585b086da63629b867ed3377a9dd53b25db21a
Nicholas Westlake <nicholasredlin@gmail.com>
2014-07-01 04:06:23 -0500

    failing test for json encoding

---
+++ test/json-replicate.js
@@ -0,0 +1,42 @@
+var level    = require('level-test')()
+var test     = require('tape')
+var sublevel = require('level-sublevel')
+var para     = require('continuable-para')
+var series   = require('continuable-series')
+
+var master   = require('../')
+
+var all      = require('./util').all
+
+function put (db, key, value) {
+  return function (cb) {
+    db.put(key, value, cb)
+  }
+}
+
+function replicate (m1, m2) {
+  return function (cb) {
+    m1.createMasterStream()
+      .pipe(m2.createSlaveStream(cb))
+  }
+}
+
+test('replicate', function (t) {
+  var db1 = sublevel(level('json_replicate_1', {valueEncoding: 'json'}))
+  var db2 = sublevel(level('json_replicate_2', {valueEncoding: 'json'}))
+
+  var m1 = master(db1, 'master', "M1")
+  var m2 = master(db2, 'master', "M2")
+
+  series(
+    put(db1, 'foo', {date: new Date()}),
+    replicate(m1, m2),
+    para(all (db1), all(db2))
+  ) (function (err, all) {
+      console.log(err, all)
+      t.notOk(err)
+      //assert that both databases are equal!
+      t.deepEqual(all[0], all[1])
+      t.end()
+    })
+})
